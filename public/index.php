c00caaa0ee4d231fa2c05e916152ef65b768b383
dimitribenhamma <168719459+dimitribenhamma@users.noreply.github.com>
2026-05-26 12:58:39 +0200

    Update index.php

--- public/index.php
+++ public/index.php
@@ -43,7 +43,7 @@
 				
 			// Affiche les erreurs mais doit être journalisé (et a enlever en prod)
 			error_reporting(E_ALL) ; 
-			ini_set('display_errors', 1) ;
+			ini_set('display_errors', 0) ;
 			ini_set('display_startup_errors', 1) ;
 
 			// Sécurité contre accès direct aux fichiers internes
