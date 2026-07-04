4e5cb5c4cc56afd5ae15fc77aae7cd8233129f90
dimitribenhamma <168719459+dimitribenhamma@users.noreply.github.com>
2026-07-04 11:16:35 +0200

    Add files via upload

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -1,7 +1,5 @@
-<!-- La page LoginController.php (gestion PHP + requêtes SQL) style K&R , indentation Ok -->
 <?php
-		// Ce code initialise une session unique et empêche d'être appelée plusieurs fois
-		if (session_status() === PHP_SESSION_NONE) {
+if (session_status() === PHP_SESSION_NONE) {
 			session_start() ;
 		}	
 		
