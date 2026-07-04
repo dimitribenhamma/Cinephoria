442bec96d41c6adcd702c3ac3e40d8f1390ad851
dimitribenhamma <168719459+dimitribenhamma@users.noreply.github.com>
2026-07-04 13:09:07 +0200

    Add files via upload

--- config/sql.php
+++ config/sql.php
@@ -1,20 +1,20 @@
 <!-- La page des requêtes SQL -->
  <?php	
 	// Our SQL queries are ready
-	$registration = 'SELECT Id FROM Client WHERE Id = :id' ; // Registration (Controller)
-	$registered = 'SELECT Id FROM Client WHERE Username = :username OR Email = :email' ; // Reservation (Controller)
-	$login = 'SELECT Id,Username,Email,Password,Role_Id FROM Client WHERE Email = :email' ; // Login (Controller)
-	$password = 'SELECT Id,Email FROM Client WHERE Email = :email' ; // Password (Controller)
-	$cookie_output = 'SELECT * FROM Cookies WHERE Client_Cookies_Id = :id' ; // Cookies (Controller)
+	$registration = 'SELECT Id FROM client WHERE Id = :id' ; // Registration (Controller)
+	$registered = 'SELECT Id FROM client WHERE Username = :username OR Email = :email' ; // Reservation (Controller)
+	$login = 'SELECT Id,Username,Email,Password,Role_Id FROM client WHERE Email = :email' ; // Login (Controller)
+	$password = 'SELECT Id,Email FROM client WHERE Email = :email' ; // Password (Controller)
+	$cookie_output = 'SELECT * FROM cookies WHERE Client_Cookies_Id = :id' ; // Cookies (Controller)
 	/* INSERT */
-	$cookies = 'INSERT INTO Cookies (Client_Cookies_Id,Consent,Cookie_Name,Username,Device,Platform,Browser,BrowserVersion,Language,Timezone, Country, City, Isp, Latitude, Longitude, Ip, Events, Visits, Role, Cookie_Date) VALUES (:Client_cookies_id, :Consent, :Cookie_name, :Username, :Device, :Platform, :Browser, :BrowserVersion, :Language, :Timezone, :Country, :City, :Isp, :Latitude, :Longitude, :Ip, :Events, :Visits, :Role, :Cookie_date)' ; // Cookie (Controller)
+	$cookies = 'INSERT INTO cookies (Client_Cookies_Id,Consent,Cookie_Name,Username,Device,Platform,Browser,BrowserVersion,Language,Timezone, Country, City, Isp, Latitude, Longitude, Ip, Events, Visits, Role, Cookie_Date) VALUES (:Client_cookies_id, :Consent, :Cookie_name, :Username, :Device, :Platform, :Browser, :BrowserVersion, :Language, :Timezone, :Country, :City, :Isp, :Latitude, :Longitude, :Ip, :Events, :Visits, :Role, :Cookie_date)' ; // Cookie (Controller)
 	$registrationController = 'INSERT INTO client (Name, Surname, Username, Password, Email, Date_Registration, Role_Id) VALUES (:name, :surname, :username, :password, :email, :date_registration, :role_id)' ; // Registration (Controller)
-	$Resa = 'INSERT INTO Booking (Client_Id, Movie_Id, Seats, Horaire, Sum, Date_Reservation) VALUES (:client_id, :movie_id, :seats, :horaire, :sum, :date_reservation)'; // 
+	$Resa = 'INSERT INTO booking (Client_Id, Movie_Id, Seats, Horaire, Sum, Date_Reservation) VALUES (:client_id, :movie_id, :seats, :horaire, :sum, :date_reservation)'; // 
 	/* DELETE */
-	$deleteCookie = 'DELETE FROM Cookies WHERE Id= :id'; // Cookies (Controller)
+	$deleteCookie = 'DELETE FROM cookies WHERE Id= :id'; // Cookies (Controller)
 	
 	// Verify below //
-	$delete = 'DELETE FROM Client WHERE Email = :email' ; // Identity (Controller)
-	$users = 'SELECT Id,Name,Surname,Username,Email,Date_Registration FROM Client' ; // Users (Server)
-	$test = 'SELECT Id,Username,Email FROM Client' ; // Tests (Server)
+	$delete = 'DELETE FROM client WHERE Email = :email' ; // Identity (Controller)
+	$users = 'SELECT Id,Name,Surname,Username,Email,Date_Registration FROM client' ; // Users (Server)
+	$test = 'SELECT Id,Username,Email FROM client' ; // Tests (Server)
 ?>
